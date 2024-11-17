869438805fda38249e12209ed75345c0b6d920fa
kircatoon <hihka302@gmail.com>
2024-11-17 20:29:28 +0300

    Create 3.4_11.py

---
+++ 3.4.Встроенные возможности по работе с коллекциями/3.4_11.py
@@ -0,0 +1,8 @@
+from itertools import product
+x = int(input())
+y = int(input())
+ln = len(str(x * y))
+for i, j in product(range(1, x + 1), range(1, y + 1)):
+    print(f'{((i - 1) * y + j):>{ln}}', end=' ')
+    if j == y:
+        print()
