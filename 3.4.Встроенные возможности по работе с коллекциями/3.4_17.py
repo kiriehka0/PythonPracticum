0a7801cb5af11d503c08a345d8bd67ee6cd87a41
kircatoon <hihka302@gmail.com>
2024-11-17 20:31:21 +0300

    Create 3.4_17.py

---
+++ 3.4.Встроенные возможности по работе с коллекциями/3.4_17.py
@@ -0,0 +1,19 @@
+from itertools import chain, combinations, product
+suit = input().strip()
+rank = input().strip()
+hand = input()
+suits = {'буби': 'бубен', 'пики': 'пик', 'трефы': 'треф', 'черви': 'червей'}
+ranks = ['10', '2', '3', '4', '5', '6', '7', '8', '9', 'валет', 'дама', 'король', 'туз']
+
+ranks.remove(rank)
+deck = product(ranks, suits.values())
+triplets = combinations(deck, 3)
+triplets = [triplet for triplet in triplets if suits[suit] in list(chain.from_iterable(triplet))]
+triplets.sort()
+print_next = False
+for triplet in triplets:
+    if print_next:
+        print(', '.join(f'{rank} {suit}' for rank, suit in triplet))
+        break
+    if ', '.join(f'{rank} {suit}' for rank, suit in triplet) == hand:
+        print_next = True
