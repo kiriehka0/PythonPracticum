a4d4d427b5c11c6da7eb6212a6883a12e7f3be7d
kircatoon <hihka302@gmail.com>
2024-11-29 00:05:48 +0300

    Update 3.5_10.py

--- Потоковый ввод(вывод). Работа с текстовыми файлами. JSON/3.5_10.py
+++ Потоковый ввод(вывод). Работа с текстовыми файлами. JSON/3.5_10.py
@@ -1,18 +1,4 @@
-import json
-
-n = input()
-n2 = input()
-a = []
-with open(n, "r", encoding="UTF-8") as f:
-    for line in f:
-        a += [int(x) for x in line.split()]
-dict = {
-    "count": len(a),
-    "positive_count": len([x for x in a if x > 0]),
-    "min": min(a),
-    "max": max(a),
-    "sum": sum(a),
-    "average": round((sum(a) / len(a)), 2),
-}
-with open(n2, "w", encoding="UTF-8") as f1:
-    json.dump(dict, f1, ensure_ascii=False, indent=4)
+a = input()
+n = int(input())
+with open(a, "r", encoding="utf-8") as f:
+    print(*f.readlines()[-n:], sep="")
