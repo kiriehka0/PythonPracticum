fd748ae9230cb1610f1dee16d7e3730a50c908e3
kircatoon <hihka302@gmail.com>
2024-11-29 00:06:23 +0300

    Update 3.5_12.py

--- Потоковый ввод(вывод). Работа с текстовыми файлами. JSON/3.5_12.py
+++ Потоковый ввод(вывод). Работа с текстовыми файлами. JSON/3.5_12.py
@@ -1,11 +1,23 @@
-import json
 from sys import stdin
-
 n = input()
-with open(n, 'r', encoding="utf-8") as f:
-    a = json.load(f)
-for x in stdin:
-    i, y = x.rstrip().split(" == ")
-    a[i] = y
-with open(n, "w", encoding="UTF-8") as f:
-    json.dump(a, f, ensure_ascii=False, indent=4, sort_keys=True)
+n1 = input()
+n3 = input()
+n4 = input()
+with open(n, "r", encoding="utf-8") as f:
+    for x in f:
+        a, a1, a2 = [], [], []
+        for i in x.split():
+            cnt = sum([i.count(str(x)) for x in range(0, 9, 2)])
+            cnt2 = sum([i.count(str(x)) for x in range(1, 10, 2)])
+            if cnt > cnt2:
+                a.append(i)
+            elif cnt < cnt2:
+                a1.append(i)
+            else:
+                a2.append(i)
+        for x, y in zip(
+            [n1, n3, n4],
+            [a, a1, a2],
+        ):
+            with open(x, "a", encoding="UTF-8") as f:
+                print(*y, file=f)
