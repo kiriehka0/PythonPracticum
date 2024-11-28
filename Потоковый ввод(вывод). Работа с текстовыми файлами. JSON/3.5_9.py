ebbe7ab2f025a9dbc984bdede7674c2f2602a549
kircatoon <hihka302@gmail.com>
2024-11-29 00:05:31 +0300

    Update 3.5_9.py

--- Потоковый ввод(вывод). Работа с текстовыми файлами. JSON/3.5_9.py
+++ Потоковый ввод(вывод). Работа с текстовыми файлами. JSON/3.5_9.py
@@ -1,4 +1,9 @@
-a = input()
-n = int(input())
-with open(a, "r", encoding="utf-8") as f:
-    print(*f.readlines()[-n:], sep="")
+n = input()
+n2 = input()
+with open(n, "r", encoding="utf-8") as f1:
+    with open(n2, "w", encoding="utf-8") as f:
+        for x in f1:
+            x = x.rstrip().replace("\t", "")
+            b = x.split()
+            if len(b) != 0:
+                print(*b, file=f)
