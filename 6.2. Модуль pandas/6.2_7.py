9b13185fd43b4b876152fb4edcb0e2d58c741711
kircatoon <hihka302@gmail.com>
2025-05-16 23:23:54 +0300

    Create 6.2_7.py

---
+++ 6.2. Модуль pandas/6.2_7.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+
+def need_to_work_better(journal: pd.DataFrame) -> pd.DataFrame:
+    filtered = journal.copy()
+    for index in journal.index:
+        if not any(journal.loc[index][1:4] == 2):
+            filtered = filtered.drop([index], axis=0)
+
+    return filtered
