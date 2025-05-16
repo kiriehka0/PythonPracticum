f7153b440a5c54bbbc681fa9b3455edf624c3582
kircatoon <hihka302@gmail.com>
2025-05-16 23:23:36 +0300

    Create 6.2_6.py

---
+++ 6.2. Модуль pandas/6.2_6.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+
+def best(journal: pd.DataFrame) -> pd.DataFrame:
+    filtered = journal.copy()
+    for index in journal.index:
+        if not all(journal.loc[index][1:4] >= 4):
+            filtered = filtered.drop([index], axis=0)
+
+    return filtered
