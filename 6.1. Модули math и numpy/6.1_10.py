75129dfa37ed7c74535d36afa0117d87e0bf76bb
kircatoon <hihka302@gmail.com>
2025-05-16 23:20:59 +0300

    Create 6.1_10.py

---
+++ 6.1. Модули math и numpy/6.1_10.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def stairs(array):
+    N = array.shape[0]
+    matrix = np.zeros((N, N), dtype="int8")
+    for i in range(N):
+        for j in range(N):
+            matrix[i][(j + i) % N] = array[j]
+
+    return matrix
