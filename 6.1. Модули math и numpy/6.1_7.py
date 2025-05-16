eff73a3ce99a5af8baa213f3d7cad577f96b9f64
kircatoon <hihka302@gmail.com>
2025-05-16 23:20:09 +0300

    Create 6.1_7.py

---
+++ 6.1. Модули math и numpy/6.1_7.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def make_board(N):
+    board = np.ones((N, N), dtype="int8")
+    for i in range(N):
+        for j in range(N):
+            board[i][j] -= (i + j) % 2
+    return board
