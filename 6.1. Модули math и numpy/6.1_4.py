c881833f6a629fb93f904d8f58bd07f35a281af4
kircatoon <hihka302@gmail.com>
2025-05-16 23:19:21 +0300

    Create 6.1_4.py

---
+++ 6.1. Модули math и numpy/6.1_4.py
@@ -0,0 +1,18 @@
+import math
+
+
+def get_geom_mean(*args):
+    result = 1
+    for x in args:
+        result *= x
+    result = math.pow(result, 1 / len(args))
+    return result
+
+
+def main():
+    numbers = [float(x) for x in input().split()]
+    print(get_geom_mean(*numbers))
+
+
+if __name__ == "__main__":
+    main()
