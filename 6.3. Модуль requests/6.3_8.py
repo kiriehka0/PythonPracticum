e1cc1eef528ea9124650c6854e58d8549d1dd639
kircatoon <hihka302@gmail.com>
2025-05-16 23:27:45 +0300

    Create 6.3_8.py

---
+++ 6.3. Модуль requests/6.3_8.py
@@ -0,0 +1,18 @@
+import requests
+
+
+def main():
+    adress = "http://" + input()
+    path = "/users"
+
+    user_dict = {
+        "username": input(),
+        "last_name": input(),
+        "first_name": input(),
+        "email": input(),
+    }
+    requests.post(adress + path, json=user_dict)
+
+
+if __name__ == "__main__":
+    main()
