557e4d0a523e76b72017464757a256f25ce12d9e
kircatoon <hihka302@gmail.com>
2025-05-16 23:26:07 +0300

    Create 6.3_2.py

---
+++ 6.3. Модуль requests/6.3_2.py
@@ -0,0 +1,18 @@
+import requests
+
+
+def main():
+
+    adress = "http://" + input()
+    
+    response_number = None
+    result_sum = 0
+    while response_number != 0:
+        response_number = int(requests.get(adress).text)
+        result_sum += response_number
+
+    print(result_sum)
+
+
+if __name__ == "__main__":
+    main()
