ef6dfe6bebfb3848d71037ce87d4da947b20c9b9
kircatoon <hihka302@gmail.com>
2025-05-16 23:28:01 +0300

    Create 6.3_9.py

---
+++ 6.3. Модуль requests/6.3_9.py
@@ -0,0 +1,20 @@
+import requests
+import sys
+
+
+def main():
+    adress = "http://" + input()
+    path = "/users/"
+    id = input()
+
+    updated_user_data = {}
+
+    for line in sys.stdin:
+        key, value = line.strip().split("=")
+        updated_user_data[key] = value
+
+    requests.put(adress + path + id, json=updated_user_data)
+
+
+if __name__ == "__main__":
+    main()
