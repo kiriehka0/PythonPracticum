28eb59b59ac81264c800d0ea519f892f87d17187
kircatoon <hihka302@gmail.com>
2025-05-16 23:27:26 +0300

    Create 6.3_7.py

---
+++ 6.3. Модуль requests/6.3_7.py
@@ -0,0 +1,23 @@
+import requests
+import sys
+
+
+def main():
+    adress = "http://" + input()
+    path = "/users/"
+    id = input()
+    template = ""
+    for line in sys.stdin:
+        template += line.strip() + "\n"
+
+    try:
+        user_dict = requests.get(adress + path + id).json()
+        for key in user_dict.keys():
+            template = template.replace(f"{{{key}}}", str(user_dict[key]))
+        print(template)
+    except ValueError:
+        print("Пользователь не найден")
+
+
+if __name__ == "__main__":
+    main()
