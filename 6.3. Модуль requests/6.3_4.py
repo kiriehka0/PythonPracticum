0aa2c12a08cb8407284d58e2b2bdcc7ef847316b
kircatoon <hihka302@gmail.com>
2025-05-16 23:26:41 +0300

    Create 6.3_4.py

---
+++ 6.3. Модуль requests/6.3_4.py
@@ -0,0 +1,17 @@
+import requests
+
+
+def main():
+    adress = "http://" + input()
+    key = input()
+
+    response_dict = requests.get(adress).json()
+
+    if key in response_dict.keys():
+        print(response_dict[key])
+    else:
+        print("No data")
+
+
+if __name__ == "__main__":
+    main()
